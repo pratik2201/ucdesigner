c3cc1f9dce18a6078f6d624b37546d650105e25a
Pratik <pratik2201.pp@gmail.com>
2023-09-09 00:00:28 +0530

    .

--- stageContent/ucLayout.uc.enumAndMore.js
+++ stageContent/ucLayout.uc.enumAndMore.js
@@ -2,7 +2,7 @@ const { uniqOpt } = require("@ucbuilder:/build/common");
 const { Rect } = require("@ucbuilder:/global/drawing/shapes");
 class treeRecord {
     constructor() { }
-    
+
     /** @type {number}  */
     index = -1;
     uniqId = "";
@@ -10,30 +10,33 @@ class treeRecord {
     _element = undefined;
     nodeType = undefined;
     /** @type {HTMLElement}  */
-    set element(val) { this._element = val;  this.nodeType = this.element.nodeType; }
+    set element(val) { this._element = val; this.nodeType = this.element.nodeType; }
     /** @type {HTMLElement}  */
     get element() { return this._element; }
 
-    /** @type {HTMLElement}  */ 
+    /** @type {HTMLElement}  */
     outputElement = undefined;
-    
-    /** @type {Rect}  */ 
+
+    /** @type {Rect}  */
     rect = undefined;
 
 
-     /** @type {HTMLElement}  */ 
-     layoutitemElement = undefined;
+    /** @type {HTMLElement}  */
+    layoutitemElement = undefined;
 
     iconFileName = "other-tags.png";
     get xName() {
-        let ele = this.element;
+        let ele = this.element; 
+        if (ele === undefined) return "";
         switch (ele.nodeType) {
             case ele.ELEMENT_NODE: return ele.hasAttribute("x-name") ? ele.getAttribute("x-name") : "";
             default: return "";
         }
     }
+    
     get nodeName() {
         let ele = this.element;
+        if (ele === undefined) return "";
         switch (ele.nodeType) {
             case ele.ELEMENT_NODE: return ele.nodeName;
             case ele.TEXT_NODE: return ele.textContent;
@@ -45,6 +48,7 @@ class treeRecord {
     /** @type {NamedNodeMap[]}  */
     get attributes() {
         let ele = this.element;
+        if (ele === undefined) return [];
         /** @type {NamedNodeMap[]}  */
         let rtrn = [];
         switch (ele.nodeType) {
@@ -65,14 +69,14 @@ class treeRecord {
     }
 }
 const ucLayoutATTR = Object.freeze({
-    SELECTED_INDEX:"is-selected",
+    SELECTED_INDEX: "is-selected",
 });
 const ucDesignerATTR = Object.freeze({
-    JSON_ROW:"x.temp-jsonperameters",
-    IGNORE_IN_DESIGNER:"ignore-in-designer",
-    
+    JSON_ROW: "x.temp-jsonperameters",
+    IGNORE_IN_DESIGNER: "ignore-in-designer",
+
     ITEM_INDEX: "ii" + uniqOpt.randomNo(),
-    UNIQID: "ui886640",/// + uniqOpt.randomNo(),
+    UNIQID: "ui" + uniqOpt.randomNo(),
     SELECTED: "sel" + uniqOpt.randomNo(),
     TEXT_NODE_TAG: "TN" + uniqOpt.randomNo(),
     transAssets: {
@@ -89,8 +93,8 @@ const ucDesignerATTR = Object.freeze({
         getMainTag(htEle) {
             switch (htEle.nodeName) {
                 case this.tag.RESIZER:
-                case this.tag.CORNER:return htEle.parentElement;
-                default:return htEle;
+                case this.tag.CORNER: return htEle.parentElement;
+                default: return htEle;
             }
         },
         /** @type {HTMLElement}  */
@@ -100,7 +104,7 @@ const ucDesignerATTR = Object.freeze({
                 case this.tag.RESIZER:
                 case this.tag.CORNER:
                 case this.tag.DRAG: return true;
-                default:return false;
+                default: return false;
             }
         }
     }
