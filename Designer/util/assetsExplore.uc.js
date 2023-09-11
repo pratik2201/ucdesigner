c027b3e8c83f81a9c447bbba9959600f33128369
Pratik <pratik2201.pp@gmail.com>
2023-09-12 00:05:01 +0530

    ...

--- Designer/util/assetsExplore.uc.js
+++ Designer/util/assetsExplore.uc.js
@@ -1,11 +1,16 @@
 const { designer } = require('./assetsExplore.uc.designer.js');
 const { previewHandler } = require('@ucdesigner:/Designer/util/assetsExplore.uc.previewHandler.js');
 const { spliterType } = require('@uccontrols:/controls/Splitter.uc.enumAndMore.js');
+const { splitHandler } = require('@ucbuilder:/global/splitHandler');
 class assetsExplore extends designer {
-    constructor() { eval(designer.giveMeHug);
-        
+    constructor() {
+        eval(designer.giveMeHug);
         this.init();
         // this.singleSplitter1.type = spliterType.COLUMN;
+        this.splHandler = new splitHandler();
+        this.splHandler.uc = this;
+        this.splHandler.spliterType = 'columns';
+        this.splHandler.init(this.container, this.container);
     }
     previewHandle = new previewHandler();
 
@@ -14,7 +19,7 @@ class assetsExplore extends designer {
     /** @type {codeFileInfo}  */
     currentFilePreview = undefined;
     init() {
-        
+
         let _this = this;
         this.previewHandle.init(_this);
         this.fileExplorer1.fillRows();
--- Designer/util/fileExplorer.uc.js
+++ Designer/util/fileExplorer.uc.js
@@ -48,7 +48,7 @@ class fileExplorer extends designer {
 
         if (!this.ucExtends.session.options.loadBySession) {
             //this.comboBox1.value = '@testnpm::/';
-            //this.fillRows('@testnpm::/');
+            this.fillRows(rootPathHandler.source[0].originalFinderText);
 
         }
 
