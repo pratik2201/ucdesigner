d3e1efe60d028b2a0288cf794e0adcb99e8ef5a6
Pratik <pratik2201.pp@gmail.com>
2023-10-03 00:31:30 +0530

    d

--- stageContent/controlInfo/resource/dgvItemRow.tpt.js
+++ stageContent/controlInfo/resource/dgvItemRow.tpt.js
@@ -1,8 +1,15 @@
 const { designer } = require('./dgvItemRow.tpt.designer.js');
-class dgvItemRow extends designer{
+class dgvItemRow extends designer {
     constructor() {
         super(arguments);
-        
+       
+        this.extended.Events.onGenerateNode = (ht, row) => {
+            let ctrls = this.getAllControls(ht);
+            ctrls.chk_allowed.addEventListener("change", (e) => {
+                row.ischecked = (ctrls.chk_allowed.checked) ? "checked" : "";
+            });
+
+        }
     }
 }
 module.exports = dgvItemRow;
