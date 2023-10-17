f5d572b7e748bc77e91e029ec24a4c7c6fee3192
Pratik <pratik2201.pp@gmail.com>
2023-10-18 01:18:46 +0530

    d

--- stageContent/controlInfo/resource/dgvItemRow.tpt.js
+++ stageContent/controlInfo/resource/dgvItemRow.tpt.js
@@ -3,7 +3,7 @@ class dgvItemRow extends designer {
     constructor() {
         super(arguments);
        
-        this.detail.extended.Events.onGenerateNode = (ht, row) => {
+         this.detail.extended.Events.onGenerateNode = (ht, row) => {
             let ctrls = this.detail.getAllControls(ht);
            
             ctrls.chk_allowed.addEventListener("change", (e) => {
