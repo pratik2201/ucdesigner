4bbde78c783cc98c0c0a17d563c72be3482eb3fc
pratik2201 <pratik2201.pp@gmail.com>
2023-11-16 00:14:33 +0530

    d

--- stageContent/controlInfo/resource/dgvFooterRow.tpt.js
+++
@@ -1,8 +0,0 @@
-const { designer } = require('./dgvFooterRow.tpt.designer.js');
-class dgvFooterRow extends designer{
-    constructor() {
-        super(arguments);
-        
-    }
-}
-module.exports = dgvFooterRow;
--- stageContent/controlInfo/resource/dgvHeaderRow.tpt.js
+++
@@ -1,8 +0,0 @@
-const { designer } = require('./dgvHeaderRow.tpt.designer.js');
-class dgvHeaderRow extends designer{
-    constructor() {
-        super(arguments);
-        
-    }
-}
-module.exports = dgvHeaderRow;
